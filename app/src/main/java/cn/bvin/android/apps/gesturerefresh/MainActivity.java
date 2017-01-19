d4c3e3720ef78254df8e23a611b795648a9bb081
bvin <bvin@live.cn>
2017-01-19 14:52:32 +0800

    update text when refresh state change

--- app/src/main/java/cn/bvin/android/apps/gesturerefresh/MainActivity.java
+++ app/src/main/java/cn/bvin/android/apps/gesturerefresh/MainActivity.java
@@ -2,6 +2,7 @@ package cn.bvin.android.apps.gesturerefresh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import cn.bvin.android.lib.widget.refresh.GestureRefreshLayout;
 
@@ -12,6 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final GestureRefreshLayout gestureRefreshLayout = (GestureRefreshLayout) findViewById(R.id.gesture_refresh_layout);
+        final TextView refreshText = (TextView) findViewById(R.id.refresh_text);
+        gestureRefreshLayout.setTranslateContent(true);//是否移动ContentView
+        gestureRefreshLayout.setOnGestureChangeListener(new GestureRefreshLayout.OnGestureStateChangeListener() {
+            @Override
+            public void onStartDrag(float startY) {
+                refreshText.setText("下拉刷新");
+            }
+
+            @Override
+            public void onDragging(float draggedDistance, float releaseDistance) {
+                refreshText.setText("下拉刷新...");
+            }
+
+            @Override
+            public void onFinishDrag(float endY) {
+                refreshText.setText("释放更新");
+            }
+        });
         gestureRefreshLayout.setOnRefreshListener(new GestureRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
