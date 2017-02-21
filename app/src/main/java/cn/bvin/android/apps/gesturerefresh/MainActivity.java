cb9286027eb52a3727a8e8a4c29eaa76c991a4de
bvin <bvin@live.cn>
2017-02-21 16:13:57 +0800

    测试禁止手势刷新

--- app/src/main/java/cn/bvin/android/apps/gesturerefresh/MainActivity.java
+++ app/src/main/java/cn/bvin/android/apps/gesturerefresh/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements GestureRefreshLay
         mRefreshText = (TextView) findViewById(R.id.refresh_text);
         mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
         mProgressBar.setMax(100);
-        mGestureRefreshLayout.setTranslateContent(false);//是否移动ContentView
+        mGestureRefreshLayout.setTranslateContent(true);//是否移动ContentView
         mGestureRefreshLayout.setOnLayoutTranslateCallback(new GestureRefreshLayout.OnLayoutTranslateCallback() {
             @Override
             public void onLayoutTranslate(int movementTop) {
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements GestureRefreshLay
                 }
             }
         });
+        mGestureRefreshLayout.setEnabled(false);
         mGestureRefreshLayout.setOnGestureChangeListener(new GestureRefreshLayout.OnGestureStateChangeListener() {
             @Override
             public void onStartDrag(float startY) {
